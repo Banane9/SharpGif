6d4daac8e0ab2f3edd7536eba4d6dac19888545e
Arne Kiesewetter <banane1996@gmx.net>
2015-04-25 14:17:33 +0200

    Implement LZW Encoding

--- SharpGif/GifLZW.cs
+++ SharpGif/GifLZW.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BitStream;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,6 +11,8 @@ namespace SharpGif
     /// </summary>
     public static class GifLZW
     {
+        private const byte maxCodeSize = 12;
+
         // The largest 12 bit value
         private const ushort maxCodeValue = 4095;
 
@@ -30,67 +33,68 @@ namespace SharpGif
             if (startCodeSize < minStartCodeSize || startCodeSize > maxStartCodeSize)
                 throw new ArgumentOutOfRangeException("startCodeSize", "The encoded start code size has to be between 2 and 8!");
 
-            var gifDataStream = GifDataStream.FromStream(stream);
-            var codeStream = new BitStream.BitStream(new MemoryStream(gifDataStream.ToArray()));
-
             var indexStream = new List<byte>();
-            var codeTable = new List<CodeTableEntry>();
-
-            var codeSize = startCodeSize + 1u;
-            var indexBuffer = new List<byte>();
-            CodeTableEntry.CodeType codeType;
-            ushort prevCode = getColorCodeCount(startCodeSize);
-            do
+            var gifDataStream = GifDataStream.FromStream(stream);
+            using (var codeStream = new BitStream.BitStream(new MemoryStream(gifDataStream.ToArray())))
             {
-                var codeBytes = new byte[(int)Math.Ceiling(codeSize / 8f)];
-                codeStream.ReadBits(codeBytes, 0, codeSize);
-                var code = BitConverter.ToUInt16(codeBytes, 0);
+                var codeTable = new List<CodeTableEntry>();
 
-                if (code < codeTable.Count)
+                var codeSize = startCodeSize + 1u;
+                var indexBuffer = new List<byte>();
+                CodeTableEntry.CodeType codeType;
+                ushort prevCode = getColorCodeCount(startCodeSize);
+                do
                 {
-                    codeType = codeTable[code].Type;
+                    var codeBytes = new byte[(int)Math.Ceiling(codeSize / 8f)];
+                    codeStream.ReadBits(codeBytes, 0, codeSize);
+                    var code = BitConverter.ToUInt16(codeBytes, 0);
 
-                    switch (codeType)
+                    if (code < codeTable.Count)
                     {
-                        case CodeTableEntry.CodeType.Color:
-                            indexStream.AddRange(codeTable[code].Colors);
+                        codeType = codeTable[code].Type;
 
-                            indexBuffer.Clear();
-                            indexBuffer.AddRange(codeTable[prevCode].Colors);
-                            indexBuffer.Add(codeTable[code].Colors[0]);
+                        switch (codeType)
+                        {
+                            case CodeTableEntry.CodeType.Color:
+                                indexStream.AddRange(codeTable[code].Colors);
 
-                            codeTable.Add(new CodeTableEntry(indexBuffer.ToArray()));
-                            break;
+                                indexBuffer.Clear();
+                                indexBuffer.AddRange(codeTable[prevCode].Colors);
+                                indexBuffer.Add(codeTable[code].Colors[0]);
 
-                        case CodeTableEntry.CodeType.Clear:
-                            indexBuffer.Clear();
-                            buildCodeTable(codeTable, startCodeSize);
-                            break;
+                                codeTable.Add(new CodeTableEntry(indexBuffer.ToArray()));
+                                break;
+
+                            case CodeTableEntry.CodeType.Clear:
+                                indexBuffer.Clear();
+                                buildCodeTable(codeTable, startCodeSize);
+                                break;
+                        }
                     }
+                    else if (code == codeTable.Count)
+                    {
+                        codeType = CodeTableEntry.CodeType.Color;
+
+                        indexBuffer.Clear();
+                        indexBuffer.AddRange(codeTable[prevCode].Colors);
+                        indexBuffer.Add(codeTable[prevCode].Colors[0]);
+
+                        codeTable.Add(new CodeTableEntry(indexBuffer.ToArray()));
+
+                        indexStream.AddRange(codeTable[code].Colors);
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException("code", "Encoded code wasn't too high. Expected: " + codeTable.Count + " or less; got: " + code);
+                    }
+
+                    if (Math.Pow(2, codeSize) <= codeTable.Count)
+                        ++codeSize;
+
+                    prevCode = code;
                 }
-                else if (code == codeTable.Count)
-                {
-                    codeType = CodeTableEntry.CodeType.Color;
-
-                    indexBuffer.Clear();
-                    indexBuffer.AddRange(codeTable[prevCode].Colors);
-                    indexBuffer.Add(codeTable[prevCode].Colors[0]);
-
-                    codeTable.Add(new CodeTableEntry(indexBuffer.ToArray()));
-
-                    indexStream.AddRange(codeTable[code].Colors);
-                }
-                else
-                {
-                    throw new ArgumentOutOfRangeException("code", "Encoded code wasn't too high. Expected: " + codeTable.Count + " or less; got: " + code);
-                }
-
-                if (codeSize <= Math.Floor(Math.Log(code + 1, 2)))
-                    ++codeSize;
-
-                prevCode = code;
+                while (codeType != CodeTableEntry.CodeType.EndOfInformation);
             }
-            while (codeType != CodeTableEntry.CodeType.EndOfInformation);
 
             return indexStream.ToArray();
         }
@@ -125,6 +129,67 @@ namespace SharpGif
         /// <param name="colorTableSize">Number of entries in the used color table.</param>
         public static void Encode(Stream stream, IEnumerable<byte> colorData, ushort colorTableSize)
         {
+            var startCodeSize = (byte)Math.Max(minStartCodeSize, Math.Ceiling(Math.Log(colorTableSize, 2)));
+
+            if (startCodeSize > maxStartCodeSize)
+                throw new ArgumentOutOfRangeException("colorTableSize", "Color Table can have a maximum of 256 entries!");
+
+            stream.WriteByte(startCodeSize);
+
+            using (var codeStream = new BitStream.BitStream(new MemoryStream()))
+            {
+                var codeTable = new Dictionary<CodeTableEntry, ushort>();
+
+                byte codeSize = maxCodeSize + 1;
+                var indexBuffer = new List<byte>();
+                foreach (var color in colorData)
+                {
+                    if (codeSize > maxCodeSize)
+                    {
+                        codeStream.WriteBits(BitConverter.GetBytes(codeTable[new CodeTableEntry(CodeTableEntry.CodeType.Clear)]), 0, maxCodeSize);
+                        buildCodeTable(codeTable, startCodeSize);
+                        codeSize = (byte)(startCodeSize + 1);
+                    }
+                    // Add reset of codeSize
+                    var prevIndexBuffer = indexBuffer.ToArray();
+
+                    indexBuffer.Add(color);
+                    var tableEntry = new CodeTableEntry(indexBuffer.ToArray());
+
+                    if (codeTable.ContainsKey(tableEntry))
+                        continue;
+
+                    codeTable.Add(tableEntry, (ushort)codeTable.Count);
+
+                    codeStream.WriteBits(BitConverter.GetBytes(codeTable[new CodeTableEntry(prevIndexBuffer)]), 0, codeSize);
+
+                    indexBuffer.Clear();
+                    indexBuffer.Add(color);
+
+                    if (Math.Pow(2, codeSize) <= codeTable.Count)
+                        ++codeSize;
+                }
+
+                codeStream.WriteBits(BitConverter.GetBytes(codeTable[new CodeTableEntry(indexBuffer.ToArray())]), 0, codeSize);
+                codeStream.WriteBits(BitConverter.GetBytes(codeTable[new CodeTableEntry(CodeTableEntry.CodeType.EndOfInformation)]), 0, codeSize);
+                codeStream.WriteBits(0, (BitNum)(BitNum.MaxValue - codeStream.BitPosition));
+
+                var codes = ((MemoryStream)codeStream.UnderlayingStream).ToArray();
+                stream.Write(codes, 0, codes.Length);
+            }
+        }
+
+        private static void buildCodeTable(Dictionary<CodeTableEntry, ushort> codeTable, byte startCodeSize)
+        {
+            codeTable.Clear();
+
+            // startCodeSize has a max of 8, so it will always fit into a byte
+            var colorCodes = getColorCodeCount(startCodeSize);
+            for (byte code = 0; code < colorCodes; ++code)
+                codeTable.Add(new CodeTableEntry(code), code);
+
+            codeTable.Add(new CodeTableEntry(CodeTableEntry.CodeType.Clear), (ushort)codeTable.Count);
+            codeTable.Add(new CodeTableEntry(CodeTableEntry.CodeType.EndOfInformation), (ushort)codeTable.Count);
         }
 
         #endregion Encode
